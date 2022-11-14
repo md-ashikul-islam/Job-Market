244a95b4649219ba1b8c02cd67e194cd315c6297
Md Ashikul Islam <md.ashikul.islam@g.bracu.ac.bd>
2022-11-14 12:26:01 +0600

    Initial State

---
+++ src/main/java/com/ashik/jobmarket/JobMarketApplication.java
@@ -0,0 +1,13 @@
+package com.ashik.jobmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashik/jobmarket/JobMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashik.jobmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
