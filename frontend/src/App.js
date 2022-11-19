b9c83792f8eaf972b345bd5b60ae79384f47a1a3
Md Ashikul Islam <md.ashikul.islam@g.bracu.ac.bd>
2022-11-19 22:39:41 +0600

    small update

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, Feed, Dashboard, Create,Login } from "./pages"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/employer" >
+          <Route path="/employer/dashboard" element={<Dashboard />}/>
+          <Route path="/employer/create" element={<Create />}/>
+          </Route>
+          <Route path="/employee/feed" element={<Feed />}/>
+          <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
