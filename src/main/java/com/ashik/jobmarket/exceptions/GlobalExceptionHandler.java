8c409264c1f680e62be5f2ebab97a30bed1748f9
Md Ashikul Islam <md.ashikul.islam@g.bracu.ac.bd>
2022-12-11 19:41:26 +0600

    Exception Handling Added

---
+++ src/main/java/com/ashik/jobmarket/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.ashik.jobmarket.exceptions;
+
+import com.ashik.jobmarket.payloads.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException exception){
+        String message = exception.getMessage();
+        ApiResponse apiResponse = new ApiResponse(message,false);
+        return new ResponseEntity<ApiResponse>(apiResponse, HttpStatus.NOT_FOUND);
+
+    }
+
+}
